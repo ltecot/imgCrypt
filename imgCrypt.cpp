61f9f7ac33ac0041ec75ad2df1688c4536ee7f65
Lucas Tecot <lucastecot@gmail.com>
2017-01-22 04:55:45 -0800

    Added jpeg support. AKA dest image for encrypt

--- imgCrypt.cpp
+++ imgCrypt.cpp
@@ -4,7 +4,6 @@
 #include <sstream>
 #include <string>
 #include <stdlib.h>
-#include <time.h>
 #include <fstream>
 
 using namespace std;
@@ -14,10 +13,10 @@ static void help()
 {
     cerr
         << "\n--------------------------------------------------------------------------" << endl
-        << "./imgCrypt D imgFile [M]" << endl
-        << "./imgCrypt E imgFile textFile [M]" << endl
-        << "Put D as first arg for decrypt, E for encrypt" << endl
-        << "Put M as last arg if you want a datamoshed image." << endl
+        << "./imgCrypt D imgFile" << endl
+        << "./imgCrypt E imgFile textFile [destImg]" << endl
+        << "Put D as first arg for decrypt, E for encrypt." << endl
+        << "Does not support lossy format output. Make sure to give a lossless format for encrypt output." << endl
         << "--------------------------------------------------------------------------"   << endl
         << endl;
 }
@@ -29,13 +28,34 @@ unsigned char whole = 255;
 unsigned char r = whole & 0xF;
 unsigned char l = whole & 0xF0;
 
+string type2str(int type) {
+  string r;
+
+  uchar depth = type & CV_MAT_DEPTH_MASK;
+  uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+  switch ( depth ) {
+    case CV_8U:  r = "8U"; break;
+    case CV_8S:  r = "8S"; break;
+    case CV_16U: r = "16U"; break;
+    case CV_16S: r = "16S"; break;
+    case CV_32S: r = "32S"; break;
+    case CV_32F: r = "32F"; break;
+    case CV_64F: r = "64F"; break;
+    default:     r = "User"; break;
+  }
+
+  r += "C";
+  r += (chans+'0');
+
+  return r;
+}
+
 int main( int argc, char* argv[])
 {
     help();
 
     tock = false;
-
-    srand (time(NULL));
 
     if(!strcmp(argv[1], "D"))
     {
@@ -83,7 +103,10 @@ int main( int argc, char* argv[])
 
         J = Encrypt(I, file);
         file.close();
-        imwrite( argv[2], J );
+        if (argc >= 5)
+            imwrite( argv[4], J );
+        else
+            imwrite( argv[2], J );
 
     }
     else
@@ -132,8 +155,9 @@ Mat& Encrypt(Mat& I, ifstream& file)
             else 
                 c = c >> 4;
 
+            //cout << p[j] << endl;
             p[j] = (l & p[j]) | (r & c);
-
+            //cout << p[j] << endl << endl;
             tock = !tock;
             
         }
